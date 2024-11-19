a6c7d5145602bceab8e29eb3cb4779dc8d2a0752
Vikas <79444074+Vikass8246@users.noreply.github.com>
2024-11-19 18:01:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,468 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <!---    For Responsive      -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';">
+  <title>ADC</title>
+  <link rel="shortcut icon" type="image/x-icon" href="img/favicon1.jpg">
+
+  <!--Owl Carousel-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" />
+
+  <!---Bootstrap CSS     -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" types="text/css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!--Custom  Style CSS     -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+
+
+  <link rel="stylesheet" href="css/style.css" types="text/css">
+  <link href="https://fonts.googleapis.com/css2?family=Flamenco&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+  <!----Start Header-->
+  <header>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light sticky-top py-lg-2 px-lg-3">
+        <div class="container-fluid">
+          <a class="navbar-brand me-5 fw" href="index.php">
+            <div class="mainheader">
+              <div class="logo">
+                <img class="logo" src="img/favicon.png" height="45px">
+              </div>
+
+          </a>
+          <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-1 mb-lg-0">
+
+              <li class="nav-item">
+                <a class="nav-link mb-1" aria-current="page" href="#home-section">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link mb-1" href="#about-section">About</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link mb-1 " href="#tests-section">Tests</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link mb-1" href="#review-section">Review</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link mb-1" href="#blog-section">Blog</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link mb-1" href="../Appoinment/index.php" target="_blank">Book Appoinment</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link mb-1" href="Registration/index.php" target="_blank">Login</a>
+              </li>
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </ul>
+
+
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+
+
+
+  <!----End Header-->
+
+  <!---Start Hero Banner-->
+  <section class="hero-banner" id="home-section">
+    <div class="container">
+      <div class="row gx-0">
+        <div class="col-nd-12">
+          <div class="banner-text-wrap">
+            <h1 class="text-white">Ashwani Diagnostic Center</h1>
+            <p class="text-white">अश्वनी डायगनोस्टिक सेंटर मे आपका स्वागत है। <br>
+              यहा आप रक्त से संबंधित किसी भी प्रकार की जांच करा सकते है। </p>
+            <a href="Appoinment/book-appointment.php" class="btn btn-white">Book Appoinment</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </section>
+  <!-----End Hero Banner>
+       
+<!-Start Features Section---->
+  <section class="features-section">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-4">
+          <div class="features-box bg1">
+            <img src="img/calender.png" class="ser" alt="">
+            <h3> 24 Hours</h3>
+            <p>We are also available for patient 24 Hours.</p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="features-box bg2">
+            <img src="img/staff.png" class="ser" alt="">
+            <h3> Professional Staff</h3>
+            <p>Here we have experienced staff member not worry in critical situation they can handle very easily.</p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="features-box bg3">
+            <img src="img/services.png" class="ser" alt="">
+            <h3> Emergency Services</h3>
+            <p>Here we are availabe anytime, anywhere for you in case any emergency situtaion regarding to collecting,
+              testing, reporting.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!--End Features Section---->
+
+  <!--Start About Section-->
+  <section class="about-section" id="about-section">
+    <div class="container">
+      <div class="row gx-5">
+
+        <div class="col-md-6">
+          <div class="about-media">
+            <img src="img/owner.jpeg" class="aboutimg" alt="">
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="about-info">
+            <h3>About Us</h3>
+            <p class="lead">Ashwani Yadav is owner of Ashwani Diagnostic Center (ADC).</p>
+
+            <p>Ashwani Diagnostic Center Opened on 15 February 2020(covid time) and it survived good due to Covid-19
+              Situation.
+            </p>
+            <a hre="#" class="btn btn-primary">Learn More</a>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--End About Section-->
+
+  <!--Start Services Section-->
+  <section class="services-section" id="tests-section">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-12">
+          <h2 class="section-title text-center"> Tests</h2>
+        </div>
+
+        <div class="col-md-4">
+          <div class="sevices-box">
+            <img src="img/basichealthcheckup.png" class="basicheal" alt="">
+            <h3> Basic Health Check-up</h3>
+            <p>CHOLESTROL, CREATINE, S.G.P.T, TSH, CBC, BLOOD SUGAR FASTING.
+              <br>
+              <s>1150/-</s> <br>
+              <span class="highlight"><b>599/-</b></span>
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="sevices-box">
+            <img src="img/Vitaminprofile.png" class="basicheal" alt="">
+            <h3> Vitamins Profile Test</h3>
+            <p>VITAMIN D3, B12.
+              <br>
+              <s>2500/-</s><br>
+              <span class="highlight"><b>1499/-</b></span>
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="sevices-box">
+            <img src="img/thyroidtest.webp" class="basicheal" alt="">
+            <h3> Thyroid Profile Test</h3>
+            <p>T3, T4, TSH<br><s>600/-</s><br>
+              <span class="highlight"><b>349/-</b></span>
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="sevices-box">
+            <img src="img/Liverprofile.png" class="basicheal" alt="">
+            <h3> Liver Profile Test</h3>
+            <p>S.G.O.T, S.G.P.T, Billirubin, Alkaline Phosphate, Protiens, ALBUMIN
+              <br><s>800/-</s><br>
+              <span class="highlight"><b>449/-</b></span>
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="sevices-box">
+            <img src="img/Premiumhealth.jpg" class="basicheal" alt="">
+            <h3> Premium Health Check-up</h3>
+            <p>CBC, ESR, Blood Sugar Random, HBA1c, TSH, Vitamin D3, C.P.K.M.B,
+              Tropnine. Lipid Profile, S.G.O.T.
+              <br><s>5600/-</s><br>
+              <span class="highlight"><b>2999/-</b></span>
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="sevices-box">
+            <img src="img/fullbodycheckup.jpg" class="basicheal" alt="">
+            <h3> Full Body Check-Up Test</h3>
+            <p>CBC, Blood Sugar Fasting, PLBS, HBA1c, Lipid Profile, Liver Profile, Kidney Profile, C.P.K.M.B, Thyroid
+              Profile,
+              Vitamin Profile, Iron Studies, HIV, Blood Group, Urin Routine.
+              <br><s>7200/-</s><br>
+              <span class="highlight"><b>3499/-</b></span>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!--End Services Section-->
+
+  <!--Start Review Section-->
+
+  <section class="review-section bg-overlay" id="review-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="section-title">Customer Review</h2>
+        </div>
+
+        <div class="col-md-12">
+
+          <div class="review-slide">
+
+            <div class="item">
+              <div class="review-box">
+                <div class="elfsight-app-0e0f33b2-c259-44b5-8d00-9c90c3daccdb">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--End Review Section-->
+
+  <!--Start Blog Section-->
+
+  <section class="blog-section" id="blog-section">
+
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="section-title text-center">Blog & News</h2>
+        </div>
+
+
+        <div class="col-md-4">
+          <div class="blog-box">
+            <div class="blog-media">
+              <img src="img/coronaguide.jpeg" alt="" />
+            </div>
+            <div class="blog-info">
+              <h3><a href="#">सुरक्षित रहें स्वस्थ रहें</a></h3>
+              <ul class="meta">
+                <li> <i class="fas fa-calendar-week" aria-hidden="true"></i> 29 Dec 2021</li>
+                <li> <i class="fas fa-user" aria-hidden="true"></i> Admin</li>
+              </ul>
+              <p>फेस मास्क और हैन्ड ग्लव्स पहने,
+                इससे आप कोरोना वायरस के संक्रमण से बच सकेंगे ।
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="blog-box">
+            <div class="blog-media">
+              <img src="img/Bloodcamp.jpeg" alt="" />
+            </div>
+            <div class="blog-info">
+              <h3><a href="#">रक्त परीक्षण शिविर</a></h3>
+              <ul class="meta">
+                <li> <i class="fas fa-calendar-week" aria-hidden="true"></i> 29 Dec 2021</li>
+                <li> <i class="fas fa-user" aria-hidden="true"></i> Admin</li>
+              </ul>
+              <p>Here we have organize the free and discount blood testing camp
+                please attend the camp on <b>13 Feb 2021.</b> Visit the camp and test your
+                blood sugar, blood group, and also additional test on have <b>%discount</b>
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-4">
+          <div class="blog-box">
+            <div class="blog-media">
+              <img src="img/coronadoc.jpeg" alt="" />
+            </div>
+            <div class="blog-info">
+              <h3><a href="#">कोरोना मे आपकी सुरक्षा</a></h3>
+              <ul class="meta">
+                <li> <i class="fas fa-calendar-week" aria-hidden="true"></i> 29 Dec 2021</li>
+                <li> <i class="fas fa-user" aria-hidden="true"></i> Admin</li>
+              </ul>
+              <p>कोरोना मे आपकी सुरक्षा के लिए हम निरंतर तैयार है।
+                आप सभी कोरोना के
+                गाइड्लाइन को ध्यान दे ओर सुरक्षित रहे.
+                धन्यवाद !!
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+    </div>
+  </section>
+
+
+  <!--End Blog Section-->
+
+  <!----Start Footer-->
+
+  <footer>
+    <div class="container-fluid">
+      <div class="row">
+
+        <div class="row-md-8">
+          <div class="footer-widget">
+            <h3>Conatact Us</h3>
+            <ul class="footer-address">
+              <li>
+                <i class="fas fa-location-arrow"></i>
+                <br>
+                <div class="col-md-6">
+                  <h4>Address</h4>
+                  <p>Kashekhar Chawl Shop No.2 Tulset Pada,
+                    Pipe Line Road Lake Road Bhandup West,
+                    Mumbai 400078.</p>
+                  <iframe class="w-100 rounded" height="320px"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.789532154092!2d72.92795621482321!3d19.16068798703935!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b9909971f7e1%3A0x8d294b8f089ff7c7!2sAshwani%20Diagnostic%20Center%20(ADC)!5e0!3m2!1sen!2sin!4v1646030476297!5m2!1sen!2sin"
+                    loading="lazy"></iframe>
+                </div>
+              </li>
+
+
+              <li>
+                <i class="fas fa-phone"></i>
+                <br>
+                <div class="col-md-4">
+                  <h4>Phone</h4>
+                  <p><a href="tel:+918828261639">8828261639</a> <span> / </span><a
+                      href="tel:+918369497998.">8369497998.</a></p>
+                </div>
+              </li>
+
+
+              <li>
+                <i class="fas fa-envelope"></i>
+                <br>
+                <div class="col-md-4">
+                  <h4>Email</h4>
+                  <p><a href="mailto:ashwanidiagnosticcenter836@gmail.com">ashwanidiagnosticcenter836@gmail.com</a></p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-2">
+          <div class="footer-widget">
+            <h4>Go Back To</h4>
+            <ul class="footer-menu">
+              <li> <a href="#home-section"> Home </a> </li>
+              <li> <a href="#about-section"> About </a> </li>
+              <li> <a href="#tests-section"> Tests </a> </li>
+              <li> <a href="#review-section"> Review </a> </li>
+              <li> <a href="#blog-section"> Blog </a> </li>
+            </ul>
+          </div>
+        </div>
+        </div>
+
+
+
+        
+
+      </div>
+    </div>
+    <br><br>
+    <div class="container-fluid">
+      <p class="m-0 text-center text-blue">Copyright &copy; Ashwani Diagnostic Center 2022</p>
+    </div>
+  </footer>
+
+
+  <!----End Footer-->
+
+  <!--Script  -->
+  <script src="js/jquery-3.4.1.min.js" types="text/javascript"> </script>
+
+  <!--Google Review Link Js-->
+  <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+
+  <!--Bootstrap JS-->
+  <script src="js/bootstrap.min.js" types="text/javascript"> </script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+    crossorigin="anonymous"></script>
+
+  <!---Custome JS-->
+  <script src="js/Login.js" types="text/javascript"> </script>
+
+
+
+</body>
+
+</html>
